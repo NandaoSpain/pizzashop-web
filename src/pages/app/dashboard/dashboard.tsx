9e252dbf76fdabc097388b372167335422af6662
Fernando Rodrigues <117524258+NandaoSpain@users.noreply.github.com>
2024-09-14 16:06:09 -0300

    dashboard cards

--- src/pages/app/dashboard/dashboard.tsx
+++ src/pages/app/dashboard/dashboard.tsx
@@ -1,13 +1,23 @@
 import { Helmet } from 'react-helmet-async'
 
+import { DayOrdersAmountCard } from './day-orders-amount-card'
+import { MonthCanceledOrdersAmountCard } from './month-canceled-orders-amount'
+import { MonthOrdersAmountCard } from './month-orders-amount-card'
+import { MounthRevenueCard } from './mounth-revenue-card'
+
 export function Dashboard() {
   return (
     <>
       <Helmet title="Dashboard" />
-      <div className='flex flex-col gap-4'>
-        <h1 className='text-3xl font-bold tracking-tight'>Dashboard</h1>
+      <div className="flex flex-col gap-4">
+        <h1 className="text-3xl font-bold tracking-tight">Dashboard</h1>
 
-        <div></div>
+        <div className="grid grid-cols-4 gap-4">
+          <MounthRevenueCard />
+          <MonthOrdersAmountCard />
+          <DayOrdersAmountCard />
+          <MonthCanceledOrdersAmountCard />
+        </div>
       </div>
     </>
   )
